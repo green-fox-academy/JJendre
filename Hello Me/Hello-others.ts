736fbb6f4c79ff51835062a9ffad1633082f8df4
JJendre <jakabe@index.hu>
2021-10-21 16:58:31 +0200

    update folder structure

--- Hello Me/Hello-others.ts
+++
@@ -1,3 +0,0 @@
-console.log('Hello, Esther!')
-console.log('Hello, Mary!')
-console.log('Hello, Joe!')
--- Hello Me/introduce-yourself.ts
+++
@@ -1,6 +0,0 @@
-let fullName:string = `Endre JAKAB`;
-console.log(fullName)
-let age:number=50;
-console.log(age)
-let high:number=1.82;
-console.log(high)
