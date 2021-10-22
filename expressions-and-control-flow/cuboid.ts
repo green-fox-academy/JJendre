1e85f51b9e10e0521b68c9bb1b48c5a14f1096e1
JJendre <jakabe@index.hu>
2021-10-22 15:11:32 +0200

    add cuboid.ts

---
+++ expressions-and-control-flow/cuboid.ts
@@ -0,0 +1,16 @@
+// Write a program that stores 3 sides of a cuboid as variables (floats)
+// The program should write the surface area and volume of the cuboid like:
+//
+// Surface Area: 600
+// Volume: 1000
+let a: number=10;
+console.log("a=" +a);
+let b: number=11;
+console.log("b=" +b);
+let c: number=12;
+console.log("c=" +c);
+let A=((a*b*2)+(a*c*2)+(b*c*2));
+console.log("Surface Area: "+A);
+let V=(a*b*c);
+console.log("Volume: "+V);
+console.log("");
