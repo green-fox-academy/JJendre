784c66062903f9f9630e8c92daf06130dd78087f
JJendre <jakabe@index.hu>
2021-10-23 11:31:34 +0200

    add parametric-average.ts

---
+++ expressions-and-control-flow/parametric-average.ts
@@ -0,0 +1,8 @@
+// Write a program that calculates the sum and the average from 1 to a given number
+// Example input: 5
+// Example output: Sum: 15, Average: 3
+let n:number=5;
+let sum=(n*(n+1))/2;
+console.log('sum 1 to ' +n+" = "+sum);
+let average=sum/n;
+console.log('average 1 to '+n+' = '+average);
