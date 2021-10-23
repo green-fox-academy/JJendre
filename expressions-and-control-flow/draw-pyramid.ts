6cf5d530201da7319e6ef565125f9ebc2b5109e4
JJendre <jakabe@index.hu>
2021-10-23 17:35:47 +0200

    add draw-pyramid.ts

---
+++ expressions-and-control-flow/draw-pyramid.ts
@@ -0,0 +1,20 @@
+// Write a program that draws a
+// pyramid like this:
+//
+//    *
+//   ***
+//  *****
+// *******
+//
+// The pyramid should have as many lines as lineCount is
+let lineCount: number = 4;
+for (let i: number = 0; i < lineCount*2; i++){
+    let single="*";
+    let space=" ";
+    let ihalf=i/2;
+    let ihalfr=Math.round(i/2);
+    let spacemult=space.repeat(((lineCount*2)-i)/2);
+    let multi =single.repeat(i);
+    if(ihalf!=ihalfr){        
+    console.log(spacemult+multi);
+    }};
