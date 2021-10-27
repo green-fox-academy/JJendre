96a50887d67871aa913bcb9cbd6521bd19e2053e
JJendre <jakabe@index.hu>
2021-10-27 22:43:43 +0200

    add sum-elements.ts

---
+++ Arrays/sum-elements.ts
@@ -0,0 +1,9 @@
+// - Create a variable named `numbers`
+//   with the following content: `[54, 23, 66, 12]`
+// - Print the sum of the second and the third element
+{let numbers:number[]=[54, 23, 66, 12,]
+ let a=(numbers[1]);
+ let b=(numbers[2]);
+ console.log('Array is: '+numbers);
+ console.log('Sum of the second and the third element is= '+(a+b));
+}
