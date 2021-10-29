10013a136c802ae5d172a64b1bc003bb8c661d6d
JJendre <jakabe@index.hu>
2021-10-29 13:14:15 +0200

    Add swap-elements.ts

---
+++ Arrays/swap-elements.ts
@@ -0,0 +1,8 @@
+// -  Create a variable named `names` with the following content:
+//   `["Arthur", "Boe", "Chloe"]`
+// -  Swap the first and the third element of `names`
+let names=['Arthur', 'Boe', 'Chloe',]; 
+let temp = names[0];
+names[0] = names[2];
+names[2] = temp;
+console.log(names);
