222294439fb6c3b2d07f8428938cf5f30a4b395c
JJendre <jakabe@index.hu>
2021-10-30 13:22:01 +0200

    update change-element.ts

--- Arrays/change-element.ts
+++ Arrays/change-element.ts
@@ -2,6 +2,14 @@
 // -  Change the value of the fourth element (8) to 4 with the `.map` method 
 // -  Print the fourth element as a test
 {let numbers:number[]=[1, 2, 3, 8, 5, 6];
-numbers = numbers.map(function(item) { return item==8 ? 4 :item ; });
+numbers = numbers.map(function(item,index) { return index==3 ? 4 :item ; });
+/*numbers = numbers.map(function(item,index) { 
+    if (index==3) { 
+        return 4
+    } 
+    else{
+        return item
+    }
+ });*/
 console.log(numbers);
 }
