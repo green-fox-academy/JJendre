3bb2480559ed494657f6bf00606b12c38c8a2d3b
JJendre <jakabe@index.hu>
2021-11-01 18:03:33 +0100

    Add sum.ts

---
+++ Arrays/sum.ts
@@ -0,0 +1,15 @@
+// Write a function called `sum()` that returns the sum of numbers from zero to the given parameter
+//let n: number = 99;
+export function sum(n: number) {
+  function sum(n: number) {
+    let list: number[] = [];
+    for (let j = 0; j <= n; j++) {
+      list.push(j);
+    }
+    let sum = list.reduce(function (a, b) {
+      return a + b;
+    });
+    console.log(sum);
+  }
+  sum(n);
+}
