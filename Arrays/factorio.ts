f56981c34d7b8ce874265af72a8ac4e20b63862d
JJendre <jakabe@index.hu>
2021-11-02 17:56:06 +0100

    update factorio.ts

--- Arrays/factorio.ts
+++ Arrays/factorio.ts
@@ -4,5 +4,12 @@ function calculateFactorial(num: number): number {
   if (num == 0) return 1;
   else return num * calculateFactorial(num - 1);
 }
-
 console.log(calculateFactorial(8));
+
+/*function factor (n:number){
+  let a =1;
+  if (n<0) {console.log("undefined")} else{
+  for (let i=1;i<=n; i++)
+   {a=a*i;} console.log(a);   }
+  };
+  factor(0);*/
