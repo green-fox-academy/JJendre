f29361200653429237d2ade5babee4d7449dc07b
JJendre <jakabe@index.hu>
2021-11-04 20:59:55 +0100

    Add simple-replace.ts

---
+++ Arrays/simple-replace.ts
@@ -0,0 +1,6 @@
+// I would like to replace "dishwasher" with "galaxy" in this example variable, but it has a problem
+// Please fix it for me!
+// Expected ouput: In a galaxy far far away
+let example: string = "In a dishwasher far far away";
+example = example.replace("dishwasher", "galaxy");
+console.log(example);
