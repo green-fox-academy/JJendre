8a748b62c5e469a0df76a2d4a059d087b3e43053
JJendre <jakabe@index.hu>
2021-11-07 19:11:28 +0100

    Add personal-finance.ts

---
+++ Arrays/personal-finance.ts
@@ -0,0 +1,42 @@
+import { sum } from "./sum";
+
+/*Create a list with the following items:
+500, 1000, 1250, 175, 800 and 120
+Create an application which prints out the answers to the following questions:
+How much did we spend?
+Which was our greatest expense?
+Which was our cheapest spending?
+What was the average amount of our spendings?*/
+let MyList: number[] = [500, 1000, 1250, 175, 800, 120];
+
+function Howmuchdidwespend(n: Array<number>) {
+  let sum = MyList.reduce(function (a, b) {
+    return a + b;
+  });
+  console.log(sum);
+}
+
+function Whichwasourgreatestexpense(n: Array<number>) {
+  MyList.sort((a, b) => a - b);
+  let biggest: number = MyList[MyList.length - 1];
+  console.log(biggest);
+}
+
+function Whichwasourcheapestspending(n: Array<number>) {
+  MyList.sort((a, b) => a - b);
+  let cheapest: number = MyList[0];
+  console.log(cheapest);
+}
+
+function Whatwastheaverageamountofourspendings(n: Array<number>) {
+  let sum = MyList.reduce(function (a, b) {
+    return a + b;
+  });
+  let average: number = sum / MyList.length;
+  console.log(average);
+}
+
+Howmuchdidwespend(MyList);
+Whichwasourgreatestexpense(MyList);
+Whichwasourcheapestspending(MyList);
+Whatwastheaverageamountofourspendings(MyList);
