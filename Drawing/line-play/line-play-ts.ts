6cb98663d05975621ddd7c6a1e6aadc17f457195
JJendre <jakabe@index.hu>
2021-11-20 15:00:51 +0100

    Update line-play

--- Drawing/line-play/line-play-ts.ts
+++ Drawing/line-play/line-play-ts.ts
@@ -2,13 +2,7 @@ const canvas = document.querySelector(".main-canvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d");
 export {};
 
-// DO NOT TOUCH THE CODE ABOVE THIS LINE
-
-// Create a function that takes 2 parameters and draws a single line
-// Parameters: the x and y coordinates of the line's starting point
-// The function shall draw a 50-long long horizontal line from that point
-// Draw at least 3 lines with that function using a loop
-function Drawpurpleline(x, y) {
+function Drawpurpleline(x: number, y: number) {
   ctx.strokeStyle = "purple";
   ctx.beginPath();
   ctx.moveTo(390, y);
@@ -22,7 +16,7 @@ for (let i = 1; i < 16; i++) {
     }
   }
 }
-function Drawgreenline(x, y) {
+function Drawgreenline(x: number, y: number) {
   ctx.strokeStyle = "green";
   ctx.beginPath();
   ctx.moveTo(10, y);
