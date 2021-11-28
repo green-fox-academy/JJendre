2dc564d598c6761044ec42f4488a3a8a1a3225c6
JJendre <jakabe@index.hu>
2021-11-28 22:45:51 +0100

    Add: decrypt-reversed

---
+++ File-manipulations/decrypt-reversed-lines/reversed-lines.ts
@@ -0,0 +1,23 @@
+// Create a method that decrypts reversed-lines.txt
+
+var fs = require('fs');
+let straightArray: any = '';
+let straightArrayfinal: any = '';
+export function reversedLines(filename: string) {
+  let array: any = [];
+  try {
+    let filedata = fs.readFileSync(filename, 'utf-8');
+    array = filedata.toString().split('');
+    straightArray = array.reverse().join('');
+    straightArray = straightArray.toString().split('\n');
+    straightArrayfinal = straightArray.reverse().join('');
+
+    return straightArrayfinal;
+  } catch {
+    return 'File not found';
+  }
+}
+console.log(reversedLines('reversed-lines.txt'));
+fs.writeFile('normalText.txt', straightArrayfinal, function (err: Error) {
+  if (err) throw '';
+});
