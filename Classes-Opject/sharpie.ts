02ea91ef6b099da078e5c66d1480230e64c9a833
JJendre <jakabe@index.hu>
2021-12-01 21:43:47 +0100

    Add: sharpie.ts

---
+++ Classes-Opject/sharpie.ts
@@ -0,0 +1,30 @@
+// Create a Sharpie class
+// We should know the followings about each sharpie:
+// color (which should be a string),
+// width (which will be a string) and the
+// inkAmount (another string)
+// We need to specify the color and the width at creation
+// Every sharpie is created with a default inkAmount value: 100
+// We can use() the sharpie objects: using it decreases inkAmount by 10
+
+export class Sharpie {
+  public color: string;
+  public width: string;
+  public inkAmount: number;
+
+  constructor(color: string, width: string, inkAmount: number = 100) {
+    this.color = color;
+    this.width = width;
+    this.inkAmount = inkAmount;
+  }
+  public use() {
+    return (this.inkAmount -= 10);
+  }
+  public getInkAmount() {
+    return (this.inkAmount += 0);
+  }
+}
+let Sharpie1 = new Sharpie('green', '5');
+Sharpie1.use();
+Sharpie1.use();
+console.log(Sharpie1);
