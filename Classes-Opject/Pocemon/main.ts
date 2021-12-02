b3cb101af4f01886be9cd42f06a58f9ff7713bfd
JJendre <jakabe@index.hu>
2021-12-02 20:33:16 +0100

    Add Pocemon

---
+++ Classes-Opject/Pocemon/main.ts
@@ -0,0 +1,26 @@
+import { Pokemon } from './Pokemon';
+let pokemonOfAsh: Pokemon[] = initializePokemon();
+// Every pokemon has a name and a type.
+// Certain types are effective against others, e.g. water is effective against fire.
+// Ash has a few pokemon.
+// A wild pokemon appeared!
+let wildPokemon: Pokemon = new Pokemon('Oddish', 'grass', 'water');
+// Which pokemon should Ash use?
+// Implement the following `chooseEffective` function which will choose the most effective pokemon for Ash
+function initializePokemon(): Pokemon[] {
+  return [
+    new Pokemon('Bulbasaur', 'grass', 'water'),
+    new Pokemon('Pikachu', 'electric', 'water'),
+    new Pokemon('Charizard', 'fire', 'grass'),
+    new Pokemon('Pidgeot', 'flying', 'fighting'),
+    new Pokemon('Kingler', 'water', 'fire'),
+  ];
+}
+function chooseEffective(ash: Pokemon[], wild: Pokemon): any {
+  for (let i: number = 0; i < ash.length; i++) {
+    if (ash[i].isEffectiveAgainst(wild)) {
+      return ash[i].name;
+    }
+  }
+}
+console.log(`I choose you!, ${chooseEffective(pokemonOfAsh, wildPokemon)}`);
