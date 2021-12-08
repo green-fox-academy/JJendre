31746fb93ba59c8eaf631a59eaff63f9adbce7ad
JJendre <jakabe@index.hu>
2021-12-08 18:17:28 +0100

    Add: bunniesAgain.ts

---
+++ recursion/bunniesAgain.ts
@@ -0,0 +1,46 @@
+// We have bunnies standing in a line numbered as 1, 2, ...
+// The "odd" bunnies (1, 3, ...) have the usual number of ears = 2.
+// However, the even bunnies (2, 4, ...) have 3 ears each
+// (because they have a raised foot). Write a recursive method
+// which accepts the number of bunnies as a parameter and returns
+// the total number of "ears" in the bunny line.
+// The method should be able to handle invalid input (e.g. negative numbers).
+function bunniesAgain(numberOfBunies: number): number {
+  if (numberOfBunies === 1) {
+    return 2;
+  } else {
+    let twoEars: number = 0;
+    let threeEars: number = 0;
+    let totalEars: number = 0;
+    function bunniestwo(numberOfBunies: number): number {
+      if (numberOfBunies <= 0) {
+        return 0;
+      } else if (numberOfBunies <= 1) {
+        //base case
+        return 2;
+      } else {
+        twoEars = 2 + bunniestwo(numberOfBunies - 1);
+        return twoEars;
+      }
+    }
+    bunniestwo(numberOfBunies);
+
+    function bunniesthree(numberOfBunies: number): number {
+      if (numberOfBunies <= 0) {
+        return 0;
+      } else if (numberOfBunies <= 1) {
+        //base case
+        return 3;
+      } else {
+        threeEars = 3 + bunniesthree(numberOfBunies - 1);
+        return threeEars;
+      }
+    }
+    bunniesthree(numberOfBunies);
+
+    totalEars = twoEars + threeEars;
+    return Math.floor(totalEars / 2);
+  }
+}
+
+console.log(bunniesAgain(5));
