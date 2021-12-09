8d364325def398da86ce53b75e5e40377b98ae8e
JJendre <jakabe@index.hu>
2021-12-09 19:30:52 +0100

    only the readFilesync in try catch

--- File-manipulations/decrypt-reversed-lines/reversed-lines.ts
+++ File-manipulations/decrypt-reversed-lines/reversed-lines.ts
@@ -1,21 +1,26 @@
 // Create a method that decrypts reversed-lines.txt
 var fs = require('fs');
-let straightArray: any = '';
-let straightArrayfinal: any = '';
 export function reversedLines(filename: string) {
+  let straightArrayfinal: any = '';
+  let filedata = '';
+  let straightArray: any = '';
   let array: any = [];
   try {
-    let filedata = fs.readFileSync(filename, 'utf-8');
-    array = filedata.toString().split('');
-    straightArray = array.reverse().join('');
-    straightArray = straightArray.toString().split(/[\n\r]+/);
-    straightArrayfinal = straightArray.reverse().join('\n');
-    return straightArrayfinal;
+    filedata = fs.readFileSync(filename, 'utf-8');
   } catch {
     return 'File not found';
   }
+  array = filedata.toString().split('');
+  straightArray = array.reverse().join('');
+  straightArray = straightArray.toString().split(/[\n\r]+/);
+  straightArrayfinal = straightArray.reverse().join('\n');
+  return straightArrayfinal;
 }
 console.log(reversedLines('reversed-lines.txt'));
-fs.writeFile('normalText.txt', straightArrayfinal, function (err: Error) {
-  if (err) throw '';
-});
+fs.writeFile(
+  'normalText.txt',
+  reversedLines('reversed-lines.txt'),
+  function (err: Error) {
+    if (err) throw '';
+  }
+);
