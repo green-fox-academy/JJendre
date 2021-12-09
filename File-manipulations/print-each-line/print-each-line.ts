c1e34341f91a853f3aa6a5ae3bd14373daca9e81
JJendre <jakabe@index.hu>
2021-12-09 19:28:30 +0100

    repair  try catch method

--- File-manipulations/print-each-line/print-each-line.ts
+++ File-manipulations/print-each-line/print-each-line.ts
@@ -4,10 +4,13 @@
 // it should print the following error message: "Unable to read file: my-file.txt"
 
 let fs = require('fs');
-fs.readFile('my-file.txt', function (err: any, data: any) {
-  if (err) console.log('Unable to read file: my-file.txt');
-  const array = data.toString().split('\n');
-  for (let i = 0; i < array.length; i++) {
-    console.log(array[i]);
-  }
-});
+let dataOfFile: string = '';
+try {
+  dataOfFile = fs.readFileSync('my-file.txt', 'utf-8');
+} catch (e) {
+  console.log('Unable to read file: my-file.txt');
+}
+const textarray: string[] = dataOfFile.toString().split('\n');
+for (let i = 0; i < textarray.length; i++) {
+  console.log(textarray[i]);
+}
