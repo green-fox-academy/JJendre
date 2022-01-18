6066999dcef6df0b577099b0e62bb6cc9119fe8a
JJendre <jakabe@index.hu>
2022-01-18 17:41:20 +0100

    code revision according to mentors advice

--- Abstracts & Interfaces/Flyable/flyable.ts
+++ Abstracts & Interfaces/Flyable/flyable.ts
@@ -6,31 +6,9 @@
 // implement your Flyable interface
 // Extend Bird from your abstract Animal class (zoo exercise)
 // implement your Flyable interface
-import { Animal } from '../Zoo/animal';
+
 export interface Flyable {
   land(): string;
   fly(): string;
   takeOff(): string;
-}
-
-class Bird extends Animal implements Flyable {
-  constructor(name: string) {
-    super(name, 1, 'female', 'bird');
-    this.name = name;
-  }
-  reproduce(): string {
-    return 'laying eggs.';
-  }
-  eating(): string {
-    return 'everything';
-  }
-  land(): string {
-    return 'lands on trees.';
-  }
-  fly(): string {
-    return `${this.name} flies naturaly.`;
-  }
-  takeOff(): string {
-    return 'takes off anywhere.';
-  }
 }
--- Abstracts & Interfaces/Flyable/vehicle.ts
+++ Abstracts & Interfaces/Flyable/vehicle.ts
@@ -1,10 +1,5 @@
 export abstract class Vehicle {
-  typeOfVehicle: string;
-  speedOfVehicle: number;
-  numberOfengines: number;
-  constructor() {
-    this.typeOfVehicle = '';
-    this.speedOfVehicle = 0;
-    this.numberOfengines = 0;
-  }
+  typeOfVehicle: string = '';
+  speedOfVehicle: number = 0;
+  numberOfengines: number = 0;
 }
